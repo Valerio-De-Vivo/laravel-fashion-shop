7dd96deeb3a9c0fb73a940c14427cf38718c1c50
Valerio <valeriodevivo90@gmail.com>
2021-03-16 17:46:04 +0100

    finish

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -4,14 +4,18 @@
 
 @section('content')
 
-    @foreach ($dresses as $dress)
+    <div class="d-flex flex-row flex-wrap">
+        @foreach ($dresses as $dress)
 
-        <div class="col-md-3">
+        <div class="col-md-3 p-5">
             <h2> {{$dress->prodotto}} </h2>
-            <h3> {{$dress->categoria}} </h3>
+            <p> {{$dress->categoria}} </p>
             <p>cod: {{$dress->cod}} </p>
             <h4>{{$dress->prezzo}} €</h4>
         </div>
         
     @endforeach
+    </div>
+
+    
 @endsection
