9b5673c385d7bf48a304d96f9e92830d547a7b7b
Valerio <valeriodevivo90@gmail.com>
2021-03-16 16:23:22 +0100

    Start with model

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,14 @@
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
+      <li class="nav-item {{ (Request::route()->getName() == 'home') ? 'active' : '' }}">
+        <a class="nav-link " href=" {{ route('home') }} ">Home</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Prodotti</a>
+      <li class="nav-item {{ (Request::route()->getName() == 'shop') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('shop') }}">Prodotti</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contatti</a>
+      <li class="nav-item {{ (Request::route()->getName() == 'contatti') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('contatti') }}">Contatti</a>
       </li>
     </ul>
   </nav>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+// Route::get('/', function () {
+//     return view('home');
+// })->name('home');
+
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/contatti', 'HomeController@contatti')->name('contatti');
+Route::get('/shop', 'HomeController@shop')->name('shop');
